841904dc3bb467aef6e1ab6d1c1be4016dec337f
MengLong <a.montoyaurbano@live.com>
2017-07-27 17:46:30 +0800

    Add project files.

---
+++ IATCSharp/Feedback.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfIATCSharp
+{
+    /// <summary>
+    /// Feedback.xaml 的交互逻辑
+    /// </summary>
+    public partial class Feedback : Window
+    {
+        delegate void MyDelegate(string value);
+        private string finaltranslationhistory = "";        
+
+        private void RefreshWindow(double width, double height)
+        {
+            Left = 0;
+            //Left = System.Windows.SystemParameters.PrimaryScreenWidth - (System.Windows.SystemParameters.PrimaryScreenWidth / 2) - width / 2;
+            Top = System.Windows.SystemParameters.PrimaryScreenHeight - height - 40 - 2;
+        }
+        public Feedback()
+        {
+            InitializeComponent();
+
+            RefreshWindow(500,170);
+            Width = System.Windows.SystemParameters.PrimaryScreenWidth; //originally this line no exist
+
+        /**/this.ShowInTaskbar = false;
+            this.Topmost = true;
+            this.MouseDown += new MouseButtonEventHandler(Window_MouseDown);
+            //this.MouseMove += new MouseEventHandler(Window_MouseMove);
+            //this.MouseLeave += new MouseEventHandler(Window_MouseLeave);
+        /**///this.txtContent.TextChanged += new TextChangedEventHandler(txtContent_changeHeight);
+        /**/this.txtContent.MaxLines = 2;
+
+            ContextMenu mMenu = new ContextMenu();
+            MenuItem closeMenu = new MenuItem();
+            closeMenu.Header = "关闭";
+            closeMenu.Click += closeMenu_Click;
+            mMenu.Items.Add(closeMenu);
+            txtContent.ContextMenu = mMenu;
+            
+            Thread receiveDataThread = new Thread(new ThreadStart(ReceiveDataFromClient));
+            receiveDataThread.IsBackground = true;
+            receiveDataThread.Start();
+        }
+
+        public void closeMenu_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        void setValue(string value)
+        {
+            //this.txtContent.Text = value;
+            
+            this.txtContent.Text = value + "\n" + finaltranslationhistory;
+            finaltranslationhistory = value + "\n" + finaltranslationhistory.Substring(0, Math.Min(500, finaltranslationhistory.Length));
+        }
+        private void ReceiveDataFromClient()
+        {
+            MyDelegate d = new MyDelegate(setValue);
+            while (true)
+            {
+                try
+                {
+                    NamedPipeServerStream _pipeServer = new NamedPipeServerStream("closePipe", PipeDirection.InOut, 2);
+                    _pipeServer.WaitForConnection();
+                    StreamReader sr = new StreamReader(_pipeServer);
+                    string recData = sr.ReadLine();
+
+                    if (recData.Length > 1)
+                    {
+                        Debug.WriteLine(recData);
+                        this.Dispatcher.Invoke(d, recData);
+                    }
+                    Thread.Sleep(1000);
+                    sr.Close();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            var ColorBrush = new SolidColorBrush(Color.FromScRgb(0, 255, 255, 255));
+            this.Background = ColorBrush;
+        }
+
+        //Window_MouseLeave
+        private void Window_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var ColorBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            this.Background = ColorBrush;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public void txtContent_changeHeight(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                if ((sender as TextBox).Text != string.Empty && (sender as TextBox).Text.Length / 22 > 3)
+                {
+
+                    int row = (sender as TextBox).Text.Length / 22 + 2;
+                    this.Height = row * 30;
+
+                    RefreshWindow(500, this.Height);
+                }
+                else
+                {
+                    this.Height = 100;
+                    this.Width = 500;
+                    RefreshWindow(500, 100);
+                }
+            }
+        }
+    }
+}
---
+++ IATCSharp/SendDataPipe.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Security.Principal;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace WpfIATCSharp
+{
+    class SendDataPipe
+    {
+        private void SendData(object data)
+        {
+            try
+            {
+                NamedPipeClientStream _pipeClient = new NamedPipeClientStream(".", "closePipe", PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.Impersonation);
+                _pipeClient.Connect();
+                StreamWriter sw = new StreamWriter(_pipeClient);
+                sw.WriteLine(data);
+                sw.Flush();
+                Thread.Sleep(1000);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public void Start(string recResult)
+        {
+            string text = recResult;
+            string final_text = "";
+
+            //Solution A:
+
+            RootObject jsonObject = JsonConvert.DeserializeObject<RootObject>(text);
+            final_text = jsonObject.trans_result.dst.ToString();
+
+            //Solution B:
+            //Dictionary<string, object>
+            //JsonObject = JsonConvert.DeserializeObject<Dictionary<string, object>>(text);
+            //Dictionary<string, object>
+            //trans_result = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonObject["trans_result"].ToString());
+            //final_text = trans_result["dst"].ToString();
+
+
+            Debug.WriteLine(final_text);
+            Thread pipeThread = new Thread(new ParameterizedThreadStart(SendData));
+            pipeThread.IsBackground = true;
+            pipeThread.Start(final_text);
+        }
+
+        //For Solution A:
+        public class TransResult
+        {
+            public string src { get; set; }
+            public string dst { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string from { get; set; }
+            public int ret { get; set; }
+            public string sid { get; set; }
+            public string to { get; set; }
+            public TransResult trans_result { get; set; }
+        }
+    }   
+}
